094e85756c2e9d060fc5323f00811468b51f7d25
Andrew Johnson <andrewmjohnson549@gmail.com>
2024-03-23 20:49:33 +0100

    Full image is now obtained

--- src/server.py
+++ src/server.py
@@ -22,14 +22,6 @@ def process_image(image_data: cv2.imread) -> List[Tuple[int, int, int, int, str]
     return [(50, 60, 100, 200, "Example")]
 
 
-def decode_image(image_data: bytes) -> cv2.imread:
-    # Convert bytes to a numpy array
-    nparr = np.frombuffer(image_data, "uint8")
-    # Decode image
-    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-    return img
-
-
 def server(host="0.0.0.0", port=5000):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((host, port))
@@ -41,15 +33,22 @@ def server(host="0.0.0.0", port=5000):
             print(f"Connected by {addr}")
 
             # Receiving image data
-            image_size: int = struct.unpack(">L", conn.recv(4))[
+            image_size = struct.unpack(">L", conn.recv(4))[
                 0
             ]  # Assuming the first 4 bytes indicate the image size
-            raw_image_byte_data: bytes = conn.recv(image_size)
+            image_data = b""
+            while len(image_data) < image_size:
+                data = conn.recv(4096)
+                if not data:
+                    break
+                image_data += data
 
-            image_data = decode_image(raw_image_byte_data)
+            # Decode image
+            nparr = np.frombuffer(image_data, np.uint8)  # type: ignore
+            image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
             # Process the image to get bounding boxes and labels
-            bounding_boxes = process_image(image_data)
+            bounding_boxes = process_image(image)
 
             # Send back the bounding boxes and labels
             response = str(bounding_boxes).encode()
